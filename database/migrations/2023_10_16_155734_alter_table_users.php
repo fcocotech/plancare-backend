1c3d5f66d1cbe5347d9432aa2e6ba5e8098c2ef0
Gerardo <g.socias29@gmail.com>
2023-10-17 00:04:06 +0800

    alter table users, added columns

---
+++ database/migrations/2023_10_16_155734_alter_table_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('sec_q1')->nullable();
+            $table->string('sec_q1_ans')->nullable();
+            $table->integer('sec_q2')->nullable();
+            $table->string('sec_q2_ans')->nullable();
+            $table->integer('sec_q3')->nullable();
+            $table->string('sec_q3_ans')->nullable();
+            $table->integer('sec_q4')->nullable();
+            $table->string('sec_q4_ans')->nullable();
+            $table->integer('sec_q5')->nullable();
+            $table->string('sec_q5_ans')->nullable();
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
