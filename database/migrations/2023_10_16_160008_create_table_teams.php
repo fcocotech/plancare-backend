a14b4e6ecf0d98e2efdac750e508a569f57cd0ae
Gerardo <g.socias29@gmail.com>
2023-10-17 00:04:19 +0800

    created table teams

---
+++ database/migrations/2023_10_16_160008_create_table_teams.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->integer('userid');
+            $table->integer('parentid');
+            $table->double('points')->default(0);
+            $table->integer('teamid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
