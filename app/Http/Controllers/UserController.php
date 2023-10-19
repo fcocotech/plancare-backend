64628ccd117f235ccc93acfa3abfbabeb8afe4a7
Gerardo <g.socias29@gmail.com>
2023-10-20 00:02:06 +0800

    save registered user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,57 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\{File, Hash};
 
 class UserController extends Controller
 {
     public function create(Request $request) { 
-        
+        $user = new User;
+        $user->address          = $request->address;
+        $user->birthdate        = $request->birthdate;
+        $user->city             = $request->city;
+        $user->email            = $request->email;
+        $user->idtype           = $request->idtype;
+        $user->mobile_number    = $request->mobile_number;
+        $user->name             = $request->name;
+        $user->nationality      = $request->nationality;
+        $user->zipcode          = $request->zipcode;
+        $user->sec_q1           = $request->sec_q1;
+        $user->sec_q2           = $request->sec_q2;
+        $user->sec_q3           = $request->sec_q3;
+        $user->sec_q4           = $request->sec_q4;
+        $user->sec_q5           = $request->sec_q5;
+        $user->sec_q1_ans = $request->sec_q1_ans;
+        $user->sec_q2_ans = $request->sec_q2_ans;
+        $user->sec_q3_ans = $request->sec_q3_ans;
+        $user->sec_q4_ans = $request->sec_q4_ans;
+        $user->sec_q5_ans = $request->sec_q5_ans;
+
+        $user->password = Hash::make($request->password);
+
+        $profile_image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $request->photoprofile));
+        $profile_path = storage_path('app/public/images/profiles/');
+        if(!File::isDirectory($profile_path)){
+            File::makeDirectory($profile_path, 0777, true, true);
+        }
+        $profile_name = time().'_'.$user->id.'_profile.png';
+        file_put_contents($profile_path.$profile_name, $profile_image);
+        $user->profile_url = env('APP_URL', '') . '/storage/images/profiles/'.$profile_name;
+
+        $id_image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $request->photoid));
+        $id_path = storage_path('app/public/images/ids/');
+        if(!File::isDirectory($id_path)){
+            File::makeDirectory($id_path, 0777, true, true);
+        }
+        $id_name = time().'_'.$user->id.'_id.png';
+        file_put_contents($id_path.$id_name, $id_image);
+        $user->idurl = env('APP_URL', '') . '/storage/images/ids/'.$id_name;
+
+        $user->save();
+        return response()->json(['status' => true, 'user' => $user]);
     }
 
-    public function update(Request $request, $user_id) {
+    public function update(Request $request, $validatedData) {
         try{
             $user = User::where('id', $user_id)->first();
             $user->address          = $request->address;
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', 'App\Http\Controllers\AuthController@login')->withoutMiddleware(['App\Http\Middleware\VerifyBearerToken']);
 // Security Question  
 Route::get('/security-questions/get-all', 'App\Http\Controllers\SecurityQuestionController@get')->withoutMiddleware(['App\Http\Middleware\VerifyBearerToken']);
+Route::post('/register/user', 'App\Http\Controllers\UserController@create')->withoutMiddleware(['App\Http\Middleware\VerifyBearerToken']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
