dea079acb2fbe2aa012ae0099ea32e35fbe52c57
Gerardo <g.socias29@gmail.com>
2023-11-25 00:32:21 +0800

    added user_commissions table and it's model and controller

---
+++ app/Models/UserCommission.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserCommission extends Model
+{
+     use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'commission_level',
+        'user_id',
+        'status'
+    ];
+}
---
+++ database/migrations/2023_11_24_143729_create_user_commissions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_commissions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('commission_level');
+            $table->integer('user_id');
+            $table->integer('commission_from');
+            $table->string('status')->nullable()->default('unreleased');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_commissions');
+    }
+};
