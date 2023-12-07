8a22d5374670a91bc0cb7020695d277fcfc87c9f
Gerardo <g.socias29@gmail.com>
2023-12-07 23:05:34 +0800

    product api for update, delete and change status

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -53,4 +53,81 @@ class ProductController extends Controller
         }
     }
 
+    public function delete(Request $request, $product_id) {
+        $product = Product::find($product_id);
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product not found!',
+            ], 404);
+        }
+
+        // Delete the product
+        $product->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product deleted successfully!',
+        ]);
+    }
+
+    public function updateStatus(Request $request, $product_id) {
+        $product = Product::find($product_id);
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product not found!',
+            ], 404);
+        }
+        if($request->has('is_active')){
+            $product->is_active = $request->is_active;
+        }
+        if($request->has('is_shop_active')){
+            $product->is_shop_active = $request->is_shop_active;
+        }
+        $product->update();
+        return response()->json([
+            'status' => true,
+            'message' => 'Product update successful!',
+        ]);
+    }
+
+    public function update(Request $request, $product_id) {
+        $product = Product::find($product_id);
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product not found!',
+            ], 404);
+        }
+
+        if($product->name != $request->name){
+            $product->name = $request->name;
+        }
+        if($product->description != $request->description){
+            $product->description = $request->description;
+        }
+        if($product->price != $request->price){
+            $product->price = $request->price;
+        }
+        
+        if($request->has('product_image') && $request->product_image != ''){
+            $proof_image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $request->product_image));
+            $proof_path = storage_path('app/public/images/products/');
+            if(!File::isDirectory($proof_path)){
+                File::makeDirectory($proof_path, 0777, true, true);
+            }
+            $proof_name = str_replace(' ', '_', $product->name).'.png';
+            file_put_contents($proof_path.$proof_name, $proof_image);
+            $product->photo_url = env('APP_URL', '') . '/storage/images/products/'.$proof_name;
+        }
+
+        
+        $product->update();
+        return response()->json([
+            'status' => true,
+            'message' => 'Product update successful!',
+        ]);
+    }
+
 }
