a7e10063f96e68e07849e35badd0160490e016be
Gerardo <g.socias29@gmail.com>
2024-01-03 01:08:07 +0800

    added migration for withdrawal_accounts

---
+++ database/migrations/2024_01_02_154950_create_withdrawal_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('withdrawal_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('type');
+            $table->string('bank_name');
+            $table->string('account_name');
+            $table->string('account_number');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('withdrawal_accounts');
+    }
+};
