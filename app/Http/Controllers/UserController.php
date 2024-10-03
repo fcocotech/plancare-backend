e879a4d60192e7154d4bb422122be5c2ee9d4dc5
fcocotech <franco@cocotechsolutions.com>
2024-10-03 11:56:43 +0800

    users card

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -140,7 +140,10 @@ class UserController extends Controller
                 $query->where('id', $categoryId);
             });
         }
-    
+        $users = DB::select('SELECT u.name,u.email,u.reference_code,u.parent_referral,u.cleared,p.name,p.price FROM `users` as u 
+join product_purchases as pp on u.id=pp.purchased_by 
+join products as p on p.id=pp.product_id 
+where u.status in (1,2) and isnull(u.deleted_at) and isnull(pp.deleted_at)and isnull(p.deleted_at) and pp.purchase_type=1 and u.is_admin<>1 and u.role_id<>3",)
         return $users;
         
     }
